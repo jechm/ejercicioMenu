ac33d5242d582a65dde0ec5aa65f277b254c677e
jechm <chemilo94@gmail.com>
2026-04-15 22:31:43 -0600

    Creacion de repositorio

---
+++ src/main/java/com/jechm/ejerciciosintecap/ejerciciomenu/EjercicioMenu.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jechm.ejerciciosintecap.ejerciciomenu;
+
+import ventanas.Principal;
+
+/**
+ *
+ * @author eyuman
+ */
+public class EjercicioMenu {
+
+    public static void main(String[] args) {
+        Principal menu = new Principal();
+        menu.setVisible(true);
+    }
+}
